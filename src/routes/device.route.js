a2112c9a4ff4d43d458535a4509a6c94863db6ba
Tomy <tomyaryafiosa@gmail.com>
2025-11-23 00:56:54 +0700

    add new feature: edit device & remove device

--- src/routes/device.route.js
+++ src/routes/device.route.js
@@ -1,9 +1,16 @@
-import { fetchAllDevices, addDevice } from '../controllers/device.controller.js';
+import {
+  fetchAllDevices,
+  addDevice,
+  modifyDevice,
+  removeDevice,
+} from '../controllers/device.controller.js';
 import express from 'express';
 
 const router = express.Router();
 
 router.get('/', fetchAllDevices);
 router.post('/', addDevice);
+router.put('/:id', modifyDevice);
+router.delete('/:id', removeDevice);
 
 export default router;
